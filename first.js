f0a78f5510763c4b347b28df751fa1d2693969fc
Dipu kumar <kumardipu845420@gmail.com>
2025-08-28 18:34:24 +0530

    footer added

--- first.js
+++ first.js
@@ -1,8 +1,17 @@
 const button ="Added a button";
 console.log(button);
+
+
 const login = "login page Add";
 console.log(login);
+
+
 const payment = "integrated the payment geteway";
 console.log(payment);
+
+
 const upi = "integrated the upi";
 console.log(upi);
+
+const footer = "footer added in our website";
+console.log(footer);
